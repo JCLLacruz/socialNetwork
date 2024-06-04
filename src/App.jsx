0f1c6c2b92bc99b455940970ddd2d078df13e7ae
Juan Carlos <juancarloslacruzlacruz@outlook.com>
2024-06-04 12:34:18 +0200

    basic structured

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Register from './views/Register/Register';
+import Login from './views/Login/Login';
+import Home from './views/Home/Home';
+import Profile from './views/Profile/Profile';
+import Header from './views/Header/Header';
+import Footer from './views/Footer/Footer';
 
 function App() {
-
-  return (
-    <>
-     <div>Home</div>
-    </>
-  )
+	return (
+		<>
+			<BrowserRouter>
+				<Header />
+				<Routes>
+					<Route path='/register' element={<Register />} />
+					<Route path='/login' element={<Login />} />
+					<Route path='/home' element={<Home />} />
+					<Route path='/profile' element={<Profile />} />
+				</Routes>
+				<Footer />
+			</BrowserRouter>
+		</>
+	);
 }
 
-export default App
+export default App;
