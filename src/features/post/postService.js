578698beaa2fe648c06a4a8acc38e430654f917d
Juan Carlos <juancarloslacruzlacruz@outlook.com>
2024-06-04 17:40:38 +0200

    post component created

--- src/features/post/postService.js
+++ src/features/post/postService.js
@@ -3,24 +3,22 @@ import axios from 'axios';
 const API_URL = 'https://serversocialnetwork.onrender.com';
 
 const createPost = async (postData) => {
-	//const token = localStorage.getItem('token');
-	const res = await axios.post(API_URL + '/posts/', postData,
-    // {
-	// 	headers: {
-	// 		Authorization: token,
-	// 	},
-	// }
+	const token = localStorage.getItem('token');
+	const res = await axios.post(API_URL + '/posts/', postData,{
+		headers: {
+			Authorization: token,
+		},
+	}
 );
 	return res.data;
 };
 const updatePost = async (postData) => {
-	//const token = localStorage.getItem('token');
-	const res = await axios.post(API_URL + '/posts/', postData, 
-    // {
-	// 	headers: {
-	// 		Authorization: token,
-	// 	},
-	// }
+	const token = localStorage.getItem('token');
+	const res = await axios.post(API_URL + '/posts/', postData, {
+		headers: {
+			Authorization: token,
+		},
+	}
 );
 	return res.data;
 };
